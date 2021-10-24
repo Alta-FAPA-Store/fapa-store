73cd43efa945f1c7b8ef77013f10227b25821eb1
fadellh <fadel.lukman.dev@gmail.com>
2021-10-24 14:27:37 +0700

    create auth with jwt

--- api/router.go
+++ api/router.go
@@ -22,6 +22,7 @@ func RegisterPath(e *echo.Echo, authController *auth.Controller, userController 
 	//user with Versioning endpoint
 	userV1 := e.Group("v1/users")
 	// userV1.GET("/:id", userController.FindUserByID)
+	userV1.Use(middleware.JWTMiddleware())
 	userV1.GET("/:username", userController.FindUserByUsername)
 	userV1.GET("", userController.FindAllUser)
 	userV1.POST("", userController.InsertUser)
@@ -29,7 +30,6 @@ func RegisterPath(e *echo.Echo, authController *auth.Controller, userController 
 
 	//pet with Versioning endpoint
 	petV1 := e.Group("v1/pets")
-	petV1.Use(middleware.JWTMiddleware())
 	petV1.GET("/:id", petController.FindPetByID)
 	petV1.GET("", petController.FindAllPet)
 	petV1.POST("", petController.InsertPet)
